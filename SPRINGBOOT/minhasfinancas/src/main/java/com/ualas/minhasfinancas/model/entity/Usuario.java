0961c1636ac638eda59cb583abae4b3b008a6b97
Ualas L Damacêno <ualasdamaceno@konstroi.com>
2024-09-25 15:17:04 -0300

    Primeiro commit

---
+++ SPRINGBOOT/minhasfinancas/src/main/java/com/ualas/minhasfinancas/model/entity/Usuario.java
@@ -0,0 +1,62 @@
+package com.ualas.minhasfinancas.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table( name = "usuario" , schema = "financas")
+public class Usuario {
+	
+	@Id
+	@Column(name = "id")//Pode ser omitido o name quando o nome da coluna e o nome da propriedade é o mesmo
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
---
+++ SPRINGBOOT/minhasfinancas/src/test/java/com/ualas/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ualas.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
